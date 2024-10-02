313ee42fc8acd43a417e6e50e7ebf0653c355ca0
Kuldip Pipaliya <kuldipem@gmail.com>
2024-10-02 12:13:18 +0530

    refactor(css): remove unused CSS files

--- build/types/index.d.ts
+++ build/types/index.d.ts
@@ -1,6 +1,3 @@
-import "./index.css";
-declare class MyLibrary {
-    constructor();
-    myMethod: () => boolean;
-}
-export default MyLibrary;
+declare const selectStyle: (condition: boolean, styleOnPass: Object, styleOnFail: Object) => Object;
+declare const buildStyle: (condition: boolean, styleKey: string, styleValueOnPass: string | number, styleValueOnFail: string | number) => Object;
+export { selectStyle, selectStyle as sest, buildStyle, buildStyle as bust, };
